4fa308b2282465bc61e1bbb8a2e4e780874345e7
leeyuan <dbdks32123@gmail.com>
2023-12-27 20:32:46 +0900

    chapter11 - debouncing

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -21,7 +21,7 @@ export default async function Page({
         <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
       </div>
       <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
-        <Search />
+        <Search placeholder={''} />
         <CreateInvoice />
       </div>
       <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,22 +1,26 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
-import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { useSearchParams, usePathname, useRouter} from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 
-export default function Search() {
+export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
 
-  function handleSearch(term: string) {
+  const handleSearch = useDebouncedCallback((term) => {
+    console.log(`Searching... ${term}`);
     const params = new URLSearchParams(searchParams);
+    
     if (term) {
       params.set('query', term);
     } else {
       params.delete('query');
     }
     replace(`${pathname}?${params.toString()}`);
-  }
+    console.log(term);
+  },  300);
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
@@ -25,6 +29,7 @@ export default function Search() {
       </label>
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+        placeholder={placeholder}
         onChange={(e) => {
           handleSearch(e.target.value);
         }}
