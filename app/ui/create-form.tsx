cece26d9813387109b45383f0ea1456369b131ba
leeyuan <dbdks32123@gmail.com>
2023-12-31 19:37:57 +0900

    chapter12 - CreateInvoice 검증

---
+++ app/ui/create-form.tsx
@@ -0,0 +1,23 @@
+'use client';
+ 
+import { customerField } from '@/app/lib/definitions';
+import Link from 'next/link';
+import {
+  CheckIcon,
+  ClockIcon,
+  CurrencyDollarIcon,
+  UserCircleIcon,
+} from '@heroicons/react/24/outline';
+import { Button } from '@/app/ui/button';
+import { createInvoice } from '@/app/lib/actions';
+ 
+export default function Form({
+  customers,
+}: {
+  customers: customerField[];
+}) {
+  return (
+    <form action={createInvoice}>
+      // ...
+  )
+}
