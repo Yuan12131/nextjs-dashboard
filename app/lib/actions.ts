510e97a9127fadc764231c4aca54bdff2922cc34
leeyuan <dbdks32123@gmail.com>
2023-12-31 19:41:19 +0900

    chapter12- CreateInvoice유형 검증

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -12,12 +12,11 @@ const FormSchema = z.object({
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
+
 export async function createInvoice(formData: FormData) {
-  const rawFormData = {
+  const { customerId, amount, status } = CreateInvoice.parse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
-  };
-  // Test it out:
-  console.log(rawFormData);
+  });
 }
