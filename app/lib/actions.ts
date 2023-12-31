6f30a4a38f28f647b6acffc7dcb6c75f8177b802
leeyuan <dbdks32123@gmail.com>
2023-12-31 19:37:56 +0900

    chapter12 - DB 스키마 정의

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,5 +1,17 @@
 'use server';
 
+import { z } from 'zod';
+
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(['pending', 'paid']),
+  date: z.string(),
+});
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
 export async function createInvoice(formData: FormData) {
   const rawFormData = {
     customerId: formData.get('customerId'),
