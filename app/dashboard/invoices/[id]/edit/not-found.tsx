191ede75a094e750526723d45c808fb5f8ac7c91
leeyuan <dbdks32123@gmail.com>
2024-01-08 23:57:28 +0900

    not found

--- app/dashboard/invoices/[id]/edit/not-found.tsx
+++ app/dashboard/invoices/[id]/edit/not-found.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import { FaceFrownIcon } from '@heroicons/react/24/outline';
- 
+
 export default function NotFound() {
   return (
     <main className="flex h-full flex-col items-center justify-center gap-2">
@@ -15,4 +15,4 @@ export default function NotFound() {
       </Link>
     </main>
   );
-}
+}                                    
